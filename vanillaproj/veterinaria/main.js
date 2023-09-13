1746b9e4670ed5b73f2de378ee439cc2120f481b
Regulvz <regulvz@gmail.com>
2023-09-13 12:53:42 -0700

    Proyecto Veterinaria en Vanilla

---
+++ vanillaproj/veterinaria/main.js
@@ -0,0 +1,8 @@
+import Header from "./components/Header";
+import Formulario from "./components/formuliario";
+import "./style.css"
+const veterianria ={
+  titulo: 'Veterinaria',
+}
+document.body.appendChild(Header(veterianria));
+document.body.appendChild(Formulario());
